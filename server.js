95c6ef12fbe06b2dd46e628465f6921516a46191
Otsuki Hitoshi <hitochan777@gmail.com>
2018-05-07 23:06:03 +0900

    Fetching Data for Pages

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app
     const server = express();
 
     server.get('/p/:id', (req, res) => {
-        const actualPage = '/post'
-        const queryParams = { title: req.params.id } 
-        app.render(req, res, actualPage, queryParams)
-    })
+      const actualPage = '/post';
+      const queryParams = {id: req.params.id};
+      app.render(req, res, actualPage, queryParams);
+    });
 
     server.get('*', (req, res) => {
       return handle(req, res);
